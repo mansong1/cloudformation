bfe0f99f0e64253e47fc66daed09cb20cd50c4cb
Martin Ansong <mansong@Martins-MacBook.local>
2020-04-05 20:23:44 +0100

    Update README.md and update shell script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Udactiy Cloud DevOps ND - Project-2 - HA web app using CloudFormation
+
+### Creating the stack 
+
+`./create-stack.sh **stack_name** **template_file** **region**`
+
+e.g.
+
+`./create-stack.sh udacitystack stack.yml us-west-2`
--- create-stack.sh
+++ create-stack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 NAME=$1
 FILE=$2
+REGION=$3
 DIR="$(PWD)"
 
-aws cloudformation create-stack
---stack-name ${NAME}
---template-body file://${DIR}/${FILE}
---region=us-west-2
+aws cloudformation create-stack \
+--stack-name ${NAME} \
+--template-body file://${DIR}/${FILE} \
+--region=${REGION}
