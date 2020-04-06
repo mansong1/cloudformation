53f2eb764b0d56169103a84ba6afe2d7a867fbd5
Martin Ansong <mansong@Martins-MacBook.local>
2020-04-06 13:51:23 +0100

    Add networking components

--- create-stack.sh
+++ create-stack.sh
@@ -4,7 +4,9 @@ FILE=$2
 REGION=$3
 DIR="$(PWD)"
 
+echo "Creating stack ${NAME}..."
 aws cloudformation create-stack \
 --stack-name ${NAME} \
 --template-body file://${DIR}/${FILE} \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
 --region ${REGION}
